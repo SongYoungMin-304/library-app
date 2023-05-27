34a15c1a69d85ce74a9b29c81f460e2840b6baf4
82108 <dudals789@naver.com>
2023-05-27 13:00:19 +0900

    유저 관련 테스트 커밋

--- src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
+++ src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
@@ -1,6 +1,15 @@
 package com.group.libraryapp.dto.user.request;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.lang.Nullable;
+
 public class UserCreateRequest {
+
+
+  public UserCreateRequest(String name, Integer age) {
+    this.name = name;
+    this.age = age;
+  }
 
   private String name;
   private Integer age;
